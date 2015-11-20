0f3e83dc5ded0bdc4aa5d37565d988d0e2bfcd09
wangbin62 <wangbin_62@126.com>
2015-11-20 11:44:17 +0800

    remove csrf protect, before_action authenticate_client! for test

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,9 @@
 #Base controller which inherited by every api controller
 class Api::V1::BaseController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-
   respond_to :json
+
+  before_action :authenticate_client!, only: [:test]
   # skip_before_filter  :verify_authenticity_token    # disable csrf token verify skips the :verify_authenticity_token filter.
   # before_filter :cors_set_headers
   #
