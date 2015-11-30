a0cf421ba0bb92996eccedbcd12a91c91edffd19
wangbin62 <wangbin_62@126.com>
2015-11-30 23:01:39 +0800

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Superadmin.create!(email: 'superadmin@zoomerrands.com', password: 'password', password_confirmation: 'password')
 Type.create!(name: 'Dog walker', comment: 'Walk dog for an hour')
 Type.create!(name: 'Cleaner', comment: 'clean house')
 Type.create!(name: 'Decorating', comment: 'decorate garden')
@@ -14,3 +13,5 @@ Type.create!(name: 'Clerical', comment: 'Work as clerck')
 Type.create!(name: 'Errands', comment: 'Some errands')
 Type.create!(name: 'Grocery', comment: 'Go grocery for client')
 Type.create!(name: 'Pets', comment: 'Care pets')
+
+Superadmin.create!(email: 'superadmin@zoomerrands.com', password: 'password', password_confirmation: 'password')
