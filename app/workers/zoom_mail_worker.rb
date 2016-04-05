333c04a25416ea65c06292d7217f3d663d1e9bb3
wangbin <wangbin_62@yahoo.com>
2016-04-05 16:32:47 +0800

    git status , fix mail worker

--- app/workers/zoom_mail_worker.rb
+++ app/workers/zoom_mail_worker.rb
@@ -4,14 +4,14 @@ class ZoomMailWorker
 	
 	def perform(task_id, user_id, type)
 		if type == 'closed'		
-			ZoomNotificationMailer.close_to_provider(task_id).deliver
-			ZoomNotificationMailer.close_to_client(task_id).deliver
+			ZoomNotificationMailer.close_to_provider(task_id).deliver if Task.find_by(id: task_id).present?
+			ZoomNotificationMailer.close_to_client(task_id).deliver if Task.find_by(id: task_id).present?
 		elsif type == 'created'
-			ZoomNotificationMailer.job_alert_to_provider(task_id, user_id).deliver	
+			ZoomNotificationMailer.job_alert_to_provider(task_id, user_id).deliver	if Task.find_by(id: task_id).present?
 		elsif type == 'updated'
-			ZoomNotificationMailer.job_updated_to_provider(task_id, user_id).deliver
+			ZoomNotificationMailer.job_updated_to_provider(task_id, user_id).deliver if Task.find_by(id: task_id).present?
 		elsif type == 'awarded'
-			ZoomNotificationMailer.job_awarded_to_provider(task_id, user_id).deliver	
+			ZoomNotificationMailer.job_awarded_to_provider(task_id, user_id).deliver	if Task.find_by(id: task_id).present?
 		end
 	end
 end
